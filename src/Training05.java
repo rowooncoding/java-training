f898fc7a533a7e0c4594532ca6fe5432171f9cf8
Rowoon Lee <dev.rowoon@gmail.com>
2023-07-26 18:53:38 +0900

    구구단 만들기

---
+++ src/Training05.java
@@ -0,0 +1,10 @@
+public class Training05 {
+    public static void main(String[] args) {
+        // 구구단 만들기
+        int number = 19;
+
+        for(int i = 1 ; i <= 19 ; i += 2) {
+            System.out.println(number + "X" + i + "=" + (number*i));
+        }
+    }
+}
