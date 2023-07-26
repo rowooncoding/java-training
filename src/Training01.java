9810af7283bfe7a1028974e0758a22960769f644
Rowoon Lee <dev.rowoon@gmail.com>
2023-07-26 18:49:15 +0900

    변수와 계산

---
+++ src/Training01.java
@@ -0,0 +1,9 @@
+public class Training01 {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        double x = 10.1;
+        int y = 2;
+
+        System.out.println((int)x * y);
+    }
+}
