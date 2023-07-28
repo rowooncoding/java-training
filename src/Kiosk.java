79837207a40255535de5ae6a0bf4b33ff01de986
Rowoon Lee <dev.rowoon@gmail.com>
2023-07-28 20:39:47 +0900

    미리 만들어 놓은 키오스크, 오더 클래스 가져오기

--- src/Kiosk.java
+++ src/Kiosk.java
@@ -1,2 +1,55 @@
 public class Kiosk {
+    public final static int key = 3;
+    private int inventory;
+
+    public Kiosk(int inventory) {
+        this.inventory = inventory;
+    }
+
+
+    public Order initOder(String menu, int count, int orderType) {
+        int price = 0;
+
+        if (menu.equals("딸기요거트")){
+            price = 4500;
+        } else if (menu.equals("카페라떼") || menu.equals("밀크티")){
+            price = 3500;
+        } else if (menu.equals("아메리카노")){
+            price = 2000;
+        } else {
+            price = 0;
+        }
+
+        if (price == 0) {
+            System.out.println("메뉴가 없습니다.");
+            return null;
+        }
+
+        if (isInventory(count)) {
+            if (orderType == 1) {
+                return new DeliveryOrder(menu, count, price);
+            } else if (orderType == 2){
+                return new TakeoutOrder(menu, count, price);
+            } else {
+                return new HereOrder(menu, count, price);
+            }
+        } else {
+            System.out.println("재고가 부족합니다.");
+            return null;
+        }
+    }
+
+
+    private boolean isInventory(int count) {
+        if (inventory >= count) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    public void subInventory(int count) {
+        inventory -= count;
+    }
 }
--- src/Order.java
+++ src/Order.java
@@ -1,2 +1,26 @@
-public class Order {
+
+public abstract class Order {
+    protected String menu;
+    protected int count;
+    protected int orderPrice;
+
+    public Order(String menu, int count, int price) {
+        this.menu = menu;
+        this.count = count;
+        setOrderPrice(price);
+    }
+
+    public abstract boolean runOrder(int deposit);
+
+
+
+    public void setOrderPrice(int price) {
+        this.orderPrice = price * count;
+        System.out.println(orderPrice + "원 입니다.");
+    }
+
+    public int getOrderPrice() {
+        return this.orderPrice;
+    }
+
 }
