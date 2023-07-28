8652209f6ae7fdee7b7932e1646eb9e6bed6f665
Rowoon Lee <dev.rowoon@gmail.com>
2023-07-28 23:43:56 +0900

    배달 주문에 대한 기능 완성

--- src/DeliveryOrder.java
+++ src/DeliveryOrder.java
@@ -1,2 +1,32 @@
-public class DeliveryOrder {
+public class DeliveryOrder extends Order{
+    protected String locate;
+
+    public DeliveryOrder(String menu, int count, int price) {
+        super(menu, count, price);
+    }
+
+    public void setLocate(String locate){
+        this.locate = locate;
+    }
+
+    @Override
+    public boolean runOrder(int deposit) {
+        int change = deposit - orderPrice;
+        if (change == 0) {
+            System.out.println(locate + " 주소로 ");
+            System.out.println(menu + " 배달이 완료 되었습니다");
+            return true;
+        } else {
+            System.out.println("금액이 부족합니다.");
+            return false;
+        }
+    }
+
+    @Override
+    public void setOrderPrice(int price) {
+        super.orderPrice = price * count;
+        System.out.println("배달비 3000원이 추가되어");
+        System.out.println(orderPrice + "원 입니다.");
+        System.out.println("배달 주문은 정확한 금액을 결정 해주세요.");
+    }
 }
--- src/RunKiosk.java
+++ src/RunKiosk.java
@@ -1,2 +1,50 @@
+import java.util.Scanner;
+
 public class RunKiosk {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("키오스크 키를 입력해주세요");
+        int key = scanner.nextInt();
+
+        if (Kiosk.key == key) {
+            System.out.println("재고개수를 입력해주세요");
+            int inventory = scanner.nextInt();
+
+            Kiosk kiosk = new Kiosk(inventory);
+
+            while(true){
+                System.out.println("1.배달 2.포장 3.매장 주문방식 번호를 입력해주세요");
+                int orderType = scanner.nextInt();
+
+                System.out.println("메뉴와 개수를 입력해주세요");
+                String menu = scanner.next();
+                if (menu.equals("종료")) {
+                    System.out.println("프로그램을 종료 합니다.");
+                    break;
+                }
+
+                int count = scanner.nextInt();
+                Order order = kiosk.initOder(menu, count, orderType);
+                int deposit = scanner.nextInt();
+                if(orderType == 1){
+                    System.out.println("주소를 입력해주세요");
+                    String locate = scanner.next();
+                    ((DeliveryOrder)order).setLocate(locate);
+                } else if (orderType == 2) {
+                    System.out.println("포장");
+                } else if (orderType == 3) {
+                    System.out.println("방문");
+                }else{
+                    continue;
+                }
+
+                if(order.runOrder(deposit)){
+                    kiosk.subInventory(count);
+                }
+
+            }
+
+
+        }
+    }
 }
